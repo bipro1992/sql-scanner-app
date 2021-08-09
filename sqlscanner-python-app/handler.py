79d44a376c656cedfa93ea5bb8d66c201c2ec4cf
bipro1992 <biprokvs@gmail.com>
2021-08-09 18:32:24 +0530

    Update handler.py

--- sqlscanner-python-app/handler.py
+++ sqlscanner-python-app/handler.py
@@ -2,6 +2,8 @@
 import boto3
 import re
 import os
+import logging
+logger = logging.getLogger()
 # connect to S3 using boto3 client
 def sqlChangesScanAndReport(event, context):
   s3=boto3.resource('s3')
@@ -15,17 +17,21 @@ def sqlChangesScanAndReport(event, context):
       fWrite=open(fileNameWithPath,'w')
       track=[]
       for line in sqlFile.get()["Body"].read().splitlines():
-        each_line = line.decode('utf-8')
-        createSearchCriteria=re.search(os.environ['CREATE_DB_CHANGES_REGEX'],each_line.lower())
-        if createSearchCriteria:
-          data=createSearchCriteria.group(7).split('.')
-          if len(data)>0:
-              track.append(re.search(os.environ['DB_OBJ_REGEX'],data[-1]).group(2))
+        try:
+          each_line = line.decode('utf-8')
+          createSearchCriteria=re.search(os.environ['CREATE_REGEX'],each_line.lower())
+          if createSearchCriteria:
+            data=createSearchCriteria.group(7).split('.')
+            if len(data)>0:
+                track.append(re.search(os.environ['OBJ_REGEX'],data[-1]).group(2))
+            else:
+                raise Exception('PATTERN MISMATCH')
           else:
-              raise Exception('PATTERN MISMATCH')
-        else:
-          alterSearchCriteria=re.search(os.environ['ALTER_DB_CHANGES_REGEX'], each_line.lower())
-          if alterSearchCriteria and (re.search(os.environ['DB_OBJ_REGEX'],alterSearchCriteria.group(7)).group(2) not in track):
-            fWrite.write(each_line)
+            alterSearchCriteria=re.search(os.environ['ALTER_REGEX'], each_line.lower())
+            if alterSearchCriteria and (re.search(os.environ['OBJ_REGEX'],alterSearchCriteria.group(7)).group(2) not in track):
+              fWrite.write(each_line)
+              logger.info(each_line)
+        except:
+          logger.error('Error occured')
       fWrite.close()
       s3.Bucket('sqlscanreport').upload_file(fileNameWithPath, fileName)
